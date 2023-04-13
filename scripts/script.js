36c1cbf8eeac6bed7ce3c02fad1327141d8b01f4
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-04-13 08:47:51 +0700

    Done

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,20 @@
-// const db = jQuery.getJSON("http://127.0.0.1:3011/data/1");
+// const res = jQuery.getJSON("http://10.100.203.78:3012/data/1");
+
 var url = 'http://10.100.203.78:3011/data/1';
 var db;
+// var dataIT;
+
+// setInterval(() => {
+//     read();
+// }, 1000);
+
+// const read = async () => {
+//     try {
+//         const response = await axios.get('http://10.100.203.78:3012/data/1');
+//         dataIT = response.data;
+//     } catch {}
+// };
+
 /**
  * It fetches the data from the url, then it converts the response to json, then it assigns the data to
  * the variable datamqtt, and finally it catches any errors and logs them to the console.
@@ -44,15 +58,15 @@ var dataold;
 var text;
 var data;
 
-var timedata = document.getElementById('timedata');
 // This is the function which handles received messages
 function myMessageArrived(message) {
     var text = JSON.parse(message.payloadString);
     data = text;
     //   console.log(data);
-    db = data;
+    db = data; 
 }
 function rd() {
+    
     const data = {
         datasets: [
             {
@@ -176,147 +190,143 @@ function rd() {
             },
         },
     };
+    
     new Chart(document.getElementById('myChart'), config);
+    const date = new Date();
+    const day = date.getDate();
 
     setInterval(function () {
-        // Nhiệt độ
+        // Pus data nhiệt độ
         //smt1
         data.datasets[0].data.push({
             x: Date.now(),
-            y: Object.values(db.nhiệt_độ.smt1)[Object.values(db.nhiệt_độ.smt1).length - 1],
+            y: Object.values(db.nhiệt_độ.smt1)[day - 1],
         });
         //smt2
         data.datasets[1].data.push({
             x: Date.now(),
-            y: Object.values(db.nhiệt_độ.smt2)[Object.values(db.nhiệt_độ.smt2).length - 1],
+            y: Object.values(db.nhiệt_độ.smt2)[day - 1],
         });
         //smt3
         data.datasets[2].data.push({
             x: Date.now(),
-            y: Object.values(db.nhiệt_độ.smt3)[Object.values(db.nhiệt_độ.smt3).length - 1],
+            y: Object.values(db.nhiệt_độ.smt3)[day - 1],
         });
         //smt4
         data.datasets[3].data.push({
             x: Date.now(),
-            y: Object.values(db.nhiệt_độ.smt4)[Object.values(db.nhiệt_độ.smt4).length - 1],
+            y: Object.values(db.nhiệt_độ.smt4)[day - 1],
         });
         //lazer5
         data.datasets[4].data.push({
             x: Date.now(),
-            y: Object.values(db.nhiệt_độ.lazer1)[Object.values(db.nhiệt_độ.lazer1).length - 1],
+            y: Object.values(db.nhiệt_độ.lazer1)[day - 1],
         });
         //lazer6
         data.datasets[5].data.push({
             x: Date.now(),
-            y: Object.values(db.nhiệt_độ.lazer2)[Object.values(db.nhiệt_độ.lazer2).length - 1],
+            y: Object.values(db.nhiệt_độ.lazer2)[day - 1],
         });
         //Coerlay7
         data.datasets[6].data.push({
             x: Date.now(),
-            y: Object.values(db.nhiệt_độ.coverlay1)[
-                Object.values(db.nhiệt_độ.coverlay1).length - 1
-            ],
+            y: Object.values(db.nhiệt_độ.coverlay1)[day - 1],
         });
         //Coverlay8
         data.datasets[7].data.push({
             x: Date.now(),
-            y: Object.values(db.nhiệt_độ.coverlay2)[
-                Object.values(db.nhiệt_độ.coverlay2).length - 1
-            ],
+            y: Object.values(db.nhiệt_độ.coverlay2)[day - 1],
         });
         //Exposure9
         data.datasets[8].data.push({
             x: Date.now(),
-            y: Object.values(db.nhiệt_độ.exposure1)[
-                Object.values(db.nhiệt_độ.exposure1).length - 1
-            ],
+            y: Object.values(db.nhiệt_độ.exposure1)[day - 1],
         });
         //Exposure10
         data.datasets[9].data.push({
             x: Date.now(),
-            y: Object.values(db.nhiệt_độ.exposure2)[
-                Object.values(db.nhiệt_độ.exposure2).length - 1
-            ],
+            y: Object.values(db.nhiệt_độ.exposure2)[day - 1],
         });
 
         //IT
-        // data.datasets[10].data.push({
-        //     x: Date.now(),
-        //     y: Object.values(db.nhiệt_độ.it1)[Object.values(db.nhiệt_độ.it1).length - 1]
-        // });
-        // //IT
-        // data.datasets[11].data.push({
-        //     x: Date.now(),
-        //     y: Object.values(db.nhiệt_độ.it2)[Object.values(db.nhiệt_độ.it2).length - 1]
-        // });
+        data.datasets[10].data.push({
+            x: Date.now(),
+            y: db.HVAC1.Temp,
+        });
+        // // //IT
+        data.datasets[11].data.push({
+            x: Date.now(),
+            y: db.HVAC2.Temp,
+        });
 
-        // Độ ẩm
+        //Pus data độ ẩm
         // smt1
         data1.datasets[0].data.push({
             x: Date.now(),
-            y: Object.values(db.độ_ẩm.smt1)[Object.values(db.độ_ẩm.smt1).length - 1],
+            y: Object.values(db.độ_ẩm.smt1)[day - 1],
         });
         //smt2
         data1.datasets[1].data.push({
             x: Date.now(),
-            y: Object.values(db.độ_ẩm.smt2)[Object.values(db.độ_ẩm.smt2).length - 1],
+            y: Object.values(db.độ_ẩm.smt2)[day - 1],
         });
         //smt3
         data1.datasets[2].data.push({
             x: Date.now(),
-            y: Object.values(db.độ_ẩm.smt3)[Object.values(db.độ_ẩm.smt3).length - 1],
+            y: Object.values(db.độ_ẩm.smt3)[day - 1],
         });
         //smt4
         data1.datasets[3].data.push({
             x: Date.now(),
-            y: Object.values(db.độ_ẩm.smt4)[Object.values(db.độ_ẩm.smt4).length - 1],
+            y: Object.values(db.độ_ẩm.smt4)[day - 1],
         });
         // lazer5
         data1.datasets[4].data.push({
             x: Date.now(),
-            y: Object.values(db.độ_ẩm.lazer1)[Object.values(db.độ_ẩm.lazer1).length - 1],
+            y: Object.values(db.độ_ẩm.lazer1)[day - 1],
         });
         //lazer6
         data1.datasets[5].data.push({
             x: Date.now(),
-            y: Object.values(db.độ_ẩm.lazer2)[Object.values(db.độ_ẩm.lazer2).length - 1],
+            y: Object.values(db.độ_ẩm.lazer2)[day - 1],
         });
         //Coerlay7
         data1.datasets[6].data.push({
             x: Date.now(),
-            y: Object.values(db.độ_ẩm.coverlay1)[Object.values(db.độ_ẩm.coverlay1).length - 1],
+            y: Object.values(db.độ_ẩm.coverlay1)[day - 1],
         });
         //Coverlay8
         data1.datasets[7].data.push({
             x: Date.now(),
-            y: Object.values(db.độ_ẩm.coverlay2)[Object.values(db.độ_ẩm.coverlay2).length - 1],
+            y: Object.values(db.độ_ẩm.coverlay2)[day - 1],
         });
         //Exposure9
         data1.datasets[8].data.push({
             x: Date.now(),
-            y: Object.values(db.độ_ẩm.exposure1)[Object.values(db.độ_ẩm.exposure1).length - 1],
+            y: Object.values(db.độ_ẩm.exposure1)[day - 1],
         });
         //Exposure10
         data1.datasets[9].data.push({
             x: Date.now(),
-            y: Object.values(db.độ_ẩm.exposure2)[Object.values(db.độ_ẩm.exposure2).length - 1],
+            y: Object.values(db.độ_ẩm.exposure2)[day - 1],
         });
 
-        //IT1
-        // data1.datasets[10].data.push({
-        //     x: Date.now(),
-        //     y: Object.values(db.độ_ẩm.it1)[Object.values(db.độ_ẩm.it1).length - 1]
-        // });
-        // //it2
-        // data1.datasets[11].data.push({
-        //     x: Date.now(),
-        //     y: Object.values(db.độ_ẩm.it2)[Object.values(db.độ_ẩm.it2).length - 1]
-        // });
+        //IT
+        data1.datasets[10].data.push({
+            x: Date.now(),
+            y: db.HVAC1.Humi,
+        });
+        // //IT
+        data1.datasets[11].data.push({
+            x: Date.now(),
+            y: db.HVAC2.Humi,
+        });
     }, 1000);
 
-    setInterval(read, 1);
+    setInterval(read, 1000);
     function read() {
-        for (var i = 0; i < 10; i++) {
+        // Hiển thị nhiệt độ độ ẩm lên màn hình
+        for (var i = 0; i < 12; i++) {
             document
                 .getElementById('left-wapper')
                 .getSVGDocument()
@@ -331,178 +341,42 @@ function rd() {
                 );
         }
 
-        if (
-            Object.values(db.nhiệt_độ.smt1)[Object.values(db.nhiệt_độ.smt1).length - 1] > 26 ||
-            Object.values(db.nhiệt_độ.smt1)[Object.values(db.nhiệt_độ.smt1).length - 1] < 20
-        ) {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill1`).style.fill = 'red';
-        } else {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill1`).style.fill = 'black';
+        // Check spec nhiệt độ
+        for (var i = 0; i < 4; i++) {
+            if (
+                config.data.datasets[i].data[config.data.datasets[i].data.length - 1].y < 20 ||
+                config.data.datasets[i].data[config.data.datasets[i].data.length - 1].y > 26
+            ) {
+                document
+                    .getElementById('left-wapper')
+                    .getSVGDocument()
+                    .getElementById(`fill${i + 1}`).style.fill = 'red';
+            } else {
+                document
+                    .getElementById('left-wapper')
+                    .getSVGDocument()
+                    .getElementById(`fill${i + 1}`).style.fill = 'black';
+            }
         }
 
-        if (
-            Object.values(db.nhiệt_độ.smt2)[Object.values(db.nhiệt_độ.smt2).length - 1] > 26 ||
-            Object.values(db.nhiệt_độ.smt2)[Object.values(db.nhiệt_độ.smt2).length - 1] < 20
-        ) {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill2`).style.fill = 'red';
-        } else {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill2`).style.fill = 'black';
+        for (var i = 4; i < 12; i++) {
+            if (
+                config.data.datasets[i].data[config.data.datasets[i].data.length - 1].y < 17 ||
+                config.data.datasets[i].data[config.data.datasets[i].data.length - 1].y > 23
+            ) {
+                document
+                    .getElementById('left-wapper')
+                    .getSVGDocument()
+                    .getElementById(`fill${i + 1}`).style.fill = 'red';
+            } else {
+                document
+                    .getElementById('left-wapper')
+                    .getSVGDocument()
+                    .getElementById(`fill${i + 1}`).style.fill = 'black';
+            }
         }
 
-        if (
-            Object.values(db.nhiệt_độ.smt3)[Object.values(db.nhiệt_độ.smt3).length - 1] > 26 ||
-            Object.values(db.nhiệt_độ.smt3)[Object.values(db.nhiệt_độ.smt3).length - 1] < 20
-        ) {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill3`).style.fill = 'red';
-        } else {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill3`).style.fill = 'black';
-        }
-
-        if (
-            Object.values(db.nhiệt_độ.smt4)[Object.values(db.nhiệt_độ.smt4).length - 1] > 26 ||
-            Object.values(db.nhiệt_độ.smt4)[Object.values(db.nhiệt_độ.smt4).length - 1] < 20
-        ) {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill4`).style.fill = 'red';
-        } else {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill4`).style.fill = 'black';
-        }
-
-        if (
-            Object.values(db.nhiệt_độ.lazer1)[Object.values(db.nhiệt_độ.lazer1).length - 1] > 23 ||
-            Object.values(db.nhiệt_độ.lazer1)[Object.values(db.nhiệt_độ.lazer1).length - 1] < 17
-        ) {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill5`).style.fill = 'red';
-        } else {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill5`).style.fill = 'black';
-        }
-
-        if (
-            Object.values(db.nhiệt_độ.lazer2)[Object.values(db.nhiệt_độ.lazer2).length - 1] > 23 ||
-            Object.values(db.nhiệt_độ.lazer2)[Object.values(db.nhiệt_độ.lazer2).length - 1] < 17
-        ) {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill6`).style.fill = 'red';
-        } else {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill6`).style.fill = 'black';
-        }
-
-        if (
-            Object.values(db.nhiệt_độ.coverlay1)[Object.values(db.nhiệt_độ.coverlay1).length - 1] >
-                23 ||
-            Object.values(db.nhiệt_độ.coverlay1)[Object.values(db.nhiệt_độ.coverlay1).length - 1] <
-                17
-        ) {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill7`).style.fill = 'red';
-        } else {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill7`).style.fill = 'black';
-        }
-
-        if (
-            Object.values(db.nhiệt_độ.coverlay2)[Object.values(db.nhiệt_độ.coverlay2).length - 1] >
-                26 ||
-            Object.values(db.nhiệt_độ.coverlay2)[Object.values(db.nhiệt_độ.coverlay2).length - 1] <
-                17
-        ) {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill8`).style.fill = 'red';
-        } else {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill8`).style.fill = 'black';
-        }
-
-        if (
-            Object.values(db.nhiệt_độ.exposure1)[Object.values(db.nhiệt_độ.exposure1).length - 1] >
-                23 ||
-            Object.values(db.nhiệt_độ.exposure1)[Object.values(db.nhiệt_độ.exposure1).length - 1] <
-                17
-        ) {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill9`).style.fill = 'red';
-        } else {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill9`).style.fill = 'black';
-        }
-
-        if (
-            Object.values(db.nhiệt_độ.exposure2)[Object.values(db.nhiệt_độ.exposure2).length - 1] >
-                23 ||
-            Object.values(db.nhiệt_độ.exposure2)[Object.values(db.nhiệt_độ.exposure2).length - 1] <
-                17
-        ) {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill10`).style.fill = 'red';
-        } else {
-            document
-                .getElementById('left-wapper')
-                .getSVGDocument()
-                .getElementById(`fill10`).style.fill = 'black';
-        }
-
-        // it
-        // if (Object.values(db.nhiệt_độ.it1)[Object.values(db.nhiệt_độ.it1).length - 1] > 23 || Object.values(db.nhiệt_độ.it1)[Object.values(db.nhiệt_độ.it1).length - 1] < 17) {
-        //     document.getElementById("left-wapper").getSVGDocument().getElementById(`fill10`).style.fill = "red";
-        // }
-        // else {
-        //     document.getElementById("left-wapper").getSVGDocument().getElementById(`fill10`).style.fill = "black";
-        // }
-        // if (Object.values(db.nhiệt_độ.it2)[Object.values(db.nhiệt_độ.it2).length - 1] > 23 || Object.values(db.nhiệt_độ.it2)[Object.values(db.nhiệt_độ.it2).length - 1] < 17) {
-        //     document.getElementById("left-wapper").getSVGDocument().getElementById(`fill10`).style.fill = "red";
-        // }
-        // else {
-        //     document.getElementById("left-wapper").getSVGDocument().getElementById(`fill10`).style.fill = "black";
-        // }
-
+        // check spec độ ẩm
         for (var i = 0; i < 12; i++) {
             if (
                 config1.data.datasets[i].data[config1.data.datasets[i].data.length - 1].y < 40 ||
